8ab1086e7b6059ecf975d45ea94a47d2484ca1fb
MdFahadFarazi <arfanfahad40@gmail.com>
2025-02-08 13:25:34 +0600

    Successfully hashed Password but Facing problem to empty the field of input in frontend

--- server.js
+++ server.js
@@ -7,6 +7,7 @@
 
 import * as http from 'http';
 import { client, connection } from './db.js';
+import bcrypt from 'bcrypt';
 
 //Database Connection 
 connection();
@@ -30,30 +31,80 @@ const server = http.createServer( async (req, res)=>{
         req.on('data', chunk => {
             body = body + chunk;
         });
-        req.on('end', ()=>{
+        req.on('end', async ()=>{
             try {
                 const parse_data = JSON.parse(body);
-                const { firstname, surname, dob, gender, email, password } = parse_data;
 
-                const query = `
-                    INSERT INTO signup_table (firstname, surname, dob, gender, email, password)
+                //Checking the url path
+                if (req.url === '/signup') {
+                    //signup logic 
+                    const { firstname, surname, dob, gender, email, password } = parse_data;
+                    
+                    //Hashing the password
+                    const salt_rounds = 10;
+                    const hashed_password = await bcrypt.hash(password, salt_rounds);
+
+                    // Data insertion
+                    const query = `
+                    INSERT INTO signup_table (firstname, surname, dob, gender, email, hashed_password)
                     VALUES ($1, $2, $3, $4, $5, $6)
-                `;
+                    `;
 
-                const values = [firstname, surname, dob, gender, email, password];
+                    const values = [firstname, surname, dob, gender, email, hashed_password];
 
-                client.query(query, values, (err, res) => {
-                    if(err) {
-                        console.error('Error executing query', err);
-                    } else {
-                        console.log('Insert successful');
-                    }
-                })
+                    // Data insertion query with placeholder 
+                    client.query(query, values, (err, res) => {
+                        if(err) {
+                            console.error('Error executing query', err);
+                        } else {
+                            console.log('Insert successful');
+                        }
+                    })
 
+                } else if (req.url === '/login') {
+                    // login logic
+                    const { email, password } = parse_data;
+
+                    // finding user in database 
+                    const query = `SELECT * FROM signup_table WHERE email = $1`;
+                    client.query(query, [email], async (err, dbRes)=> {
+                        if (err) {
+                            console.error('Error executing query: ', err.message);
+                            res.writeHead(500, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ message: 'An error occured.' }));
+                            return; 
+                        }
+
+                        const user = dbRes.rows[0];
+                        if(!user) {
+                            res.writeHead(404, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ message: 'User not found' }));
+                            return;
+                        }
+
+                        //Now, comparing the password 
+                        const is_match = await bcrypt.compare(password, user.hashed_password);
+
+                        if (is_match) {
+                            res.writeHead(200, { 'Content-Type': 'application/json' });
+                            res.end(JSON.stringify({ message: 'Login Successful' }));
+                        } else {
+                            res.writeHead(401, { 'Content-Type': 'applicatin/json' });
+                            res.end(JSON.stringify({ message: 'Invalid credentials. '}));
+                        }
+                    });
+                } else {
+                    //invalid route 
+                    res.writeHead(404, { 'Content-Type': 'applicaton/json' });
+                    res.end(JSON.stringify({ message: 'route not found.' }));
+                }
             } catch(err) {
-
+                console.error('Failed: ', err.message);
             }
         })
+    } else {
+        res.writeHead(405, { 'Content-Type': 'applicaton/json' });
+        res.end(JSON.stringify({ message: 'method not allowed.' }));
     }
 });
 
